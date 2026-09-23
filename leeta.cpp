aa37194086852d8435266cfd649a72fde821217d
ismail <ismail.kd06@gmail.com>
2026-09-23 18:48:11 +0100

    wiiw

---
+++ leeta.cpp
@@ -0,0 +1,25 @@
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+
+    int getSum(const vector<int>& v ,int start, int end ){
+        int s=0;
+        if (start>end) return 0;
+        for(int i=start; i<=end; ++i){
+            s+=v[i];
+
+        }
+        return s;
+    }
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> res(n);
+        res[0]=getSum(nums,1,n-1)+(1-n)*nums[0];
+        for(int i=0; i<n-1;++i){
+            res[i+1]=res[i]+(2*i-n+2)*(nums[i+1]-nums[i]);}
+        return res;}
+};
