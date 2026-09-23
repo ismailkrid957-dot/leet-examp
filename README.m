ac79a53d9c0de69ed5337cabc767fcb4556e8c58
ismail <ismail.kd06@gmail.com>
2026-09-23 19:02:06 +0100

    crushtest

---
+++ README.m
@@ -0,0 +1,5 @@
+# LeetCode Solutions
+
+| # | Problem | Difficulty | Language |
+|---|---------|-----------|----------|
+| 1685 | [Sum of Absolute Differences]("https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array/description/") | Medium | C++ |
